27080b8d46730b5708acaeea984e4bf9c4474c9a
vividswan <vividswan@naver.com>
2020-09-09 11:34:05 +0900

    Initial commit

---
+++ src/test/java/com/board/vividswan/VividswanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.vividswan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VividswanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
