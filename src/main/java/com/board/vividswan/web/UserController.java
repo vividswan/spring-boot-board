facc81dc3e5ea89332a78d48dc2c150af45092ed
Soohwan Park <vividswan@naver.com>
2020-09-20 23:25:40 +0900

    Update UserController.java

--- src/main/java/com/board/vividswan/web/UserController.java
+++ src/main/java/com/board/vividswan/web/UserController.java
@@ -2,6 +2,7 @@ package com.board.vividswan.web;
 
 import com.board.vividswan.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -10,6 +11,9 @@ public class UserController {
 
     @PostMapping("/create")
     public String create(User user){
+        System.out.println(user);
         return "index";
     }
+
+
 }
